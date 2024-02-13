c70ec8c17e569053a9fbb070576adc4c9c87cb1d
Seyma000 <serterseyma254@gmail.com>
2024-02-13 15:27:31 +0300

    Finding Prime Number

---
+++ src/week1/primeNumber.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class primeNumber {
+    public static void main(String[] args) {
+
+        for (int num=2; num<100; num++){
+            boolean result= true;
+            for (int i=2; i<num; i++){
+                if (num % i == 0) {
+                    result = false;
+                    break;
+                }
+            }
+            if (result==true){
+                System.out.println(num+ " is a prime number");
+            }
+        }
+    }
+}
