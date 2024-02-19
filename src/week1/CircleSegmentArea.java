fa7c0343a2d5e6bdffdf72c5bfdf34d6c59fe0c4
Seyma000 <serterseyma254@gmail.com>
2024-02-19 16:31:10 +0300

    Find the area of circle segment

---
+++ src/week1/CircleSegmentArea.java
@@ -0,0 +1,21 @@
+package week1;
+
+import java.util.Scanner;
+
+public class CircleSegmentArea {
+    public static void main(String[] args) {
+        double pi= 3.14;
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter radius");
+        int r= input.nextInt();
+        System.out.println("Enter center angle measurement");
+        int a= input.nextInt();
+
+        // Formul
+        double area= (pi * (r*r) * a) / 360;
+
+        System.out.println("Area of circle segment: "+area);
+
+    }
+}
