6236fd149302ac53ec3f4d1239f1c2225daf8164
Seyma000 <serterseyma254@gmail.com>
2024-02-19 14:02:17 +0300

    Taximeter Calculation

--- src/week1/Taximeter.java
+++ src/week1/Taximeter.java
@@ -4,17 +4,17 @@ import  java.util.Scanner;
 public class Taximeter {
     public static void main(String[] args) {
         double distance;
-        double km_amount=20;
-        double opening_fee=25;
+        double km_amount=2.20;
+        double opening_fee=10;
 
         Scanner input= new Scanner(System.in);
         System.out.print("Enter the total distance traveled: ");
         distance= input.nextDouble();
 
         double total_fee=(distance*km_amount)+opening_fee;
-        double min_wage= 100.0;
+        double min_wage= 20.0;
 
-        double result= (distance<=10? min_wage : total_fee );
+        double result= (total_fee<=20 ? min_wage : total_fee );
         System.out.print("Total Taximeter Amount: "+ result);
 
     }
