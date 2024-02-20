2064d4154f54133654497c78cc6b81f6db5be2c1
Seyma000 <serterseyma254@gmail.com>
2024-02-21 02:09:18 +0300

    Inverted Triangle Plotting

---
+++ src/week1/invertedTriangle.java
@@ -0,0 +1,19 @@
+package week1;
+import java.util.Scanner;
+
+public class invertedTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Getting value from user
+        System.out.print("Enter the base of the triangle: ");
+        int n = input.nextInt();
+
+        // Triangle Drawing
+        for(int i = n; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
