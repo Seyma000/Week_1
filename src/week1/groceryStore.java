8f4ad5f05e69968fbd77e400b92ef99d839f6e09
Seyma000 <serterseyma254@gmail.com>
2024-02-20 10:49:43 +0300

    Grocery Store Program

--- src/week1/groceryStore.java
+++ src/week1/groceryStore.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class groceryStore {
     public static void main(String[] args){
 
-        // dedfine values
+        // define values
         double apple,pears,tomato,eggplant, banana; //  represents how many kg the customer wants to buy.
+        double appleKg=3.67 ,pearsKg=2.14 ,tomatoKg=1.11 ,eggplantKg=5.00, bananaKg=0.95 ;
         Scanner input = new Scanner(System.in);
 
         // take values from the user
@@ -20,7 +21,7 @@ public class groceryStore {
         System.out.print("How many kilos of eggplant: ");
         eggplant = input.nextDouble();
 
-        double total = (pears * 2.14)+(apple * 3.67)+(tomato * 1.11)+(banana * 0.95)+(eggplant *5.00);
+        double total = (pears * pearsKg)+(apple * appleKg)+(tomato * tomatoKg)+(banana * bananaKg)+(eggplant *eggplantKg);
 
         System.out.print("total amount :  " + total);
 
