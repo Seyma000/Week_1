d18bbbb497eef116be23417914745b41261d8ae3
Seyma000 <serterseyma254@gmail.com>
2024-02-21 00:51:04 +0300

    Sorting Numbers from Smallest to Largest

---
+++ src/week1/SortingNumbers.java
@@ -0,0 +1,42 @@
+package week1;
+
+import java.util.Scanner;
+
+public class SortingNumbers {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1= input.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2= input.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3= input.nextInt();
+
+        if (num1<num2 && num1<num3){
+            if (num2<num3){
+                System.out.println("num1<num2<num3");
+            }
+            else {
+                System.out.println("num1<num3<num2");
+            }
+        }
+        else if (num2<num1 && num2<num3) {
+            if (num1<num3){
+                System.out.println("num2<num1<num3");
+            }
+            else {
+                System.out.println("num2<num3<num1");
+            }
+        }
+        else {
+            if (num1<num2){
+                System.out.println("num3<num1<num2");
+            }
+            else {
+                System.out.println("num3<num2<num1");
+            }
+        }
+
+    }
+}
