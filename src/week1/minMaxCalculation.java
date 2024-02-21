572354c20453308e442ab4834a566e850ce3d27f
Seyma000 <serterseyma254@gmail.com>
2024-02-21 03:19:42 +0300

    Finding Min and Max Values from Entered Numbers

--- src/week1/minMaxCalculation.java
+++ src/week1/minMaxCalculation.java
@@ -2,7 +2,27 @@ package week1;
 
 import java.util.Scanner;
 public class minMaxCalculation {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("How many numbers will you enter: ");
+        int n= input.nextInt();
 
+        System.out.print("Enter number 1: ");
+        int min= input.nextInt();
+        int max= min;
 
+        for (int i=2 ; i<=n ; i++){
+            System.out.print("Enter number "+i+": ");
+            int number= input.nextInt();
 
+            if (number<min){
+                min= number;
+            }
+            else if (number>max){
+                max= number;
+            }
+        }
+        System.out.println("Smallest number is: "+min);
+        System.out.println("Largest number is: "+max);
+    }
 }
