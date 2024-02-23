4d5bc567750d5760486877fa27791dc87c7a46fa
Seyma000 <serterseyma254@gmail.com>
2024-02-23 19:03:19 +0300

    Calculating Exponents with Recursive Methods

---
+++ src/week2/recExponents.java
@@ -0,0 +1,22 @@
+package week2;
+
+import java.util.Scanner;
+
+public class recExponents {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number= input.nextInt();
+        System.out.print("Enter the power: ");
+        int power= input.nextInt();
+
+        System.out.print("Result is: ");
+        System.out.print(exponentiation(number, power));
+
+    }
+    public static int exponentiation (int number, int power){
+        if (power==0)
+            return 1;
+        return exponentiation(number, power-1);
+    }
+}
