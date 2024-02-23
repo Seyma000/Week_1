47326b2b86c07dbaf85727ae7b932b88ccbef429
Seyma000 <serterseyma254@gmail.com>
2024-02-23 23:40:21 +0300

    Finding Prime Number

--- src/week2/recPrimeNumber.java
+++ src/week2/recPrimeNumber.java
@@ -4,22 +4,19 @@ import java.util.Scanner;
 
 public class recPrimeNumber {
     // Method to check whether it is a prime number or not
-    public static int isPrime(int number, int check) {
-        // If the return 0, the number is not prime
+    public static String isPrime(int number, int check) {
         if (number < 2)
-            return 0;
-        // If the return 1, the number is prime
+            return "The number is not a Prime Number";
         if (check == 1)
-            return 1;
+            return "The number is a Prime Number";
         if (number % check == 0)
-            return 0;
+            return "The number is not a Prime Number";
         return isPrime(number, check - 1);
     }
 
     public static void main(String[] args) {
-        // Get value from user
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a number: ");
         int number = input.nextInt();
         // Print result
         System.out.println(isPrime(number, number / 2));
