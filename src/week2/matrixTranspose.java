55ff6ca761d02464fa6c22cee2a75c37ea1fbc3d
Seyma000 <serterseyma254@gmail.com>
2024-02-28 21:19:14 +0300

    Finding matrix transpose

---
+++ src/week2/matrixTranspose.java
@@ -0,0 +1,32 @@
+package week2;
+
+public class matrixTranspose {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+
+        System.out.println("Matrix is: ");
+        for (int i = 0; i <array.length; i++)
+        {
+            for (int j = 0; j<array[i].length;  j++)
+            {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println( "");
+        }
+
+        System.out.println("Transpoze is: ");
+        for (int i = 0; i < array[0].length; i++)
+        {
+            for (int j = 0; j < array.length; j++)
+            {
+                System.out.print(array[j][i] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
